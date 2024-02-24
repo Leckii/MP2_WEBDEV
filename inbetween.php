086f9b092afa9928e0b39ecf0b1e38d6c2585b35
Lec <150683104+Leckii@users.noreply.github.com>
2024-02-24 16:45:36 +0800

    Update inbetween.php

--- inbetween.php
+++ inbetween.php
@@ -24,7 +24,7 @@
 
     if (isset($_POST['deal'])) {
         $playerNumber = generateRandomNumber();
-        
+
         if ($_POST['deal'] === "deal") {
             $win = ($playerNumber > $number1 && $playerNumber < $number2 || $playerNumber > $number2 && $playerNumber < $number1);
 
@@ -62,8 +62,10 @@
 
     <h1>In-Between Game</h1>
 
-    <p>Random Numbers: <?php echo "<input type='hidden' name='number1' value='$number1'>"; ?> and <?php echo "<input type='hidden' name='number2' value='$number2'>"; ?></p>
-    
+    <p>Random Numbers: <?php echo $number1; ?> and <?php echo $number2; ?></p>
+    <p>Your number is: <?php echo $playerNumber; ?></p>
+    <p><?php echo $resultMessage; ?></p>
+
     <form method="post">
         <button type="submit" name="deal" value="deal">Deal</button>
         <button type="submit" name="nodeal" value="nodeal">No Deal</button>
