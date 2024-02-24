f02750f43d410957598ef1694fc7e881cbe0faff
Lec <150683104+Leckii@users.noreply.github.com>
2024-02-24 15:13:19 +0800

    made a starting page

--- index.php
+++ index.php
@@ -4,46 +4,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>In-Between Game</title>
+    <title>In-Between</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 
 <body>
-
-    <?php
-    function generateRandomNumber()
-    {
-        return rand(1, 10); // You can adjust the range based on your preference
-    }
-
-    $number1 = generateRandomNumber();
-    $number2 = generateRandomNumber();
-    $playerNumber = null;
-    $resultMessage = '';
-
-    if (isset($_POST['deal'])) {
-        $playerNumber = generateRandomNumber();
-
-        if ($playerNumber > $number1 && $playerNumber < $number2) {
-            $resultMessage = 'Congratulations! You win!';
-        } else {
-            $resultMessage = 'Sorry, you lose. Try again!';
-        }
-    }
-    ?>
-
-    <h1>In-Between Game</h1>
-
-    <p>Random Numbers: <?php echo $number1; ?> and <?php echo $number2; ?></p>
-
-    <form method="post">
-        <button type="submit" name="deal">Deal</button>
+    <h1>IN-BETWEEN SIR</h1>
+    <form action="inbetween.php" method="post">
+        <button class="btn btn-primary">PLAY</button>
     </form>
-
-    <?php if ($playerNumber !== null) : ?>
-        <p>Your Number: <?php echo $playerNumber; ?></p>
-        <p><?php echo $resultMessage; ?></p>
-    <?php endif; ?>
-
 </body>
 
 </html>
