2a296ab289ffb6c39ce2e52115f5b288c97d1603
xerexxa <152241326+xerexxa@users.noreply.github.com>
2024-02-27 19:02:25 +0800

    re-commit

--- index.php
+++ index.php
@@ -6,13 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>In-Between</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <h1>IN-BETWEEN SIR</h1>
     <form action="inbetween.php" method="post">
-        <button class="btn btn-primary" name="play" value="play">PLAY</button>
+        <button class="wrapper" name="play" value="play">PLAY</button>
     </form>
 </body>
 
