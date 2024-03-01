2ddf9b55f51b4f82bee4a6ef4d1d3cec955883ce
Lec <150683104+Leckii@users.noreply.github.com>
2024-03-01 16:16:39 +0800

    css of the index update

--- index.php
+++ index.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>In-Between</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="indexstyle.css">
 </head>
 
 <body>
-    <img src="images/knight_logo.png" alt="knightlogo" class="logo">
+    <img src="images/logo_knight.png" alt="knightlogo" class="logo">
     <form action="inbetween.php" method="post">
         <button class="wrapper" name="play" value="play">PLAY</button>
     </form>
