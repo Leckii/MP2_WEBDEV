7616982c557ba73ef0291f7409e23e4d5a87f8c0
Lec <150683104+Leckii@users.noreply.github.com>
2024-03-01 16:54:03 +0800

    first page update

--- index.php
+++ index.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Knights of the In-Between</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="indexstyle.css">
+    <link rel="stylesheet" href="indexstyles.css">
 </head>
 
 <body>
-    <br><br><br>
+    <br><br>
     <img src="images/final_logo.png" alt="knightlogo" class="logo">
     <br><br>
     <form action="inbetween.php" method="post" class="center">
