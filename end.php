951c05519d9d59a9009bfe44f9710b4ef25913b1
xerexxa <152241326+xerexxa@users.noreply.github.com>
2024-03-02 08:20:38 +0800

    removed animation

--- end.php
+++ end.php
@@ -5,28 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="endstyles.css">
-<style>
-    .animation-container {
-            position: fixed;
-            bottom: 0;
-            left: 50%;
-            transform: translateX(-50%);
-            width: 100px; /* Adjust the width and height according to your animation size */
-            height: 100px;
-            background-image: url('images/ATTACK.png'); /* Path to your spritesheet */
-    }
 
-            @keyframes play-attack-animation {
-            <?php
-            $frame_width_percentage = 100 / 7; // Assuming there are 7 frames horizontally
-            for ($i = 0; $i < 7; $i++) {
-                $frame_position = $i * $frame_width_percentage;
-                echo "$frame_position% { background-position: -$i * 100px 0; }\n";
-            }
-            ?>
-            }
-
-</style>
     <title>Document</title>
     
 </head>
@@ -38,15 +17,10 @@
         }
 
         if ($_SESSION['totalPoints'] < 0) {
-            // Points are negative, display DEATH animation
-            echo '<div class="animation-container" style="animation: play-death-animation 1s steps(<?php echo $frame_count; ?>) infinite; background-image: url(\'images/DEATH.png\');"></div>';
             echo '<h1 class="center">GAME OVER</h1>';
         } else {
-            // Points are not negative, display ATTACK animation
-            echo '<div class="animation-container" style="animation: play-attack-animation 1s steps(<?php echo $frame_count; ?>) infinite; background-image: url(\'images/ATTACK.png\');"></div>';
             echo '<h1 class="center">YOU WIN</h1>';
         }
-
         
         echo '<p class="center">Points Earned: ' . $_SESSION['totalPoints'] . '</p>';
 
